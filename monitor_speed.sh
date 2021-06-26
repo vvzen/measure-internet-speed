7b4264498da3f3a22b661a673b1afe9d5ead00ac
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2021-06-26 12:43:04 +0100

    Minor fixes/improvements

--- monitor_speed.sh
+++ monitor_speed.sh
@@ -3,10 +3,19 @@
 # seconds
 interval=900
 
-while [ 1 ]; do
+if [ ! `command -v speedtest-cli` ]; then
+	echo "Please install speedtest-cli before running this utility!"
+	echo "--> https://github.com/sivel/speedtest-cli"
+	exit 1
+fi
 
+if [ ! -d data ]; then
+	mkdir data
+fi
+
+while [ 1 ]; do
 	today=`date "+%d_%m_%Y"`
-	output_file=speed_test_monitor_$today.csv
+	output_file="data/speed_test_monitor_$today.csv"
 
 	if [ ! -f $output_file ]; then
 		echo "Creating csv file for $today"
